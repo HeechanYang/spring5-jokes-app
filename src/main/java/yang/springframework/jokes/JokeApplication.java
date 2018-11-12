04964711e0520bd0dfb1154a1e92e7a0cd7d40c6
HeechanYang <yhc944@gmail.com>
2018-11-12 14:06:08 +0900

    [ADD] Make bean using Configuration using XML

--- src/main/java/yang/springframework/jokes/JokeApplication.java
+++ src/main/java/yang/springframework/jokes/JokeApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ComponentScan(value = {"yang.springframework.jokes"}) //default is base project package like this.
+@ImportResource("classpath:chuck-config.xml")
 public class JokeApplication {
 
     public static void main(String[] args) {
